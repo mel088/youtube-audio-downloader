7fe300d98df9183bc29304fbeac099ab693a721d
mel088 <mnwilliams31@gmail.com>
2026-01-12 23:34:21 -0800

    cleaned

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from pathlib import Path
 from pydub import AudioSegment
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
- 
+
+VIDEO_FOLDER = "temp_videos"
+AUDIO_FOLDER = "audios"
 url = input("Enter URL: ")
- 
-yt = YouTube(url, on_progress_callback = on_progress)
+
+yt = YouTube(url, on_progress_callback=on_progress)
 title = yt.title
 
 # Get video
 ys = yt.streams.get_highest_resolution()
 file_name = f"{title}.mp4"
-video_path = f"downloads/{file_name}"
-print(video_path)
+video_path = f"{VIDEO_FOLDER}/"
 
 # Download video
 ys.download(output_path=video_path)
@@ -22,9 +23,11 @@ ys.download(output_path=video_path)
 # Save audio
 current_dir = Path(__file__).resolve().parent
 print(f"Current directory: {current_dir}")
-full_video_path = f"{current_dir.parent}/Youtube-Video-Downloader/{video_path}/{file_name}"
+full_video_path = (
+    f"{current_dir.parent}/youtube-audio-downloader/{video_path}/{file_name}"
+)
 audio = AudioSegment.from_file(full_video_path, format="mp4")
-audio.export(f"audios/{title}_audio.mp3", format="mp3")
+audio.export(f"{AUDIO_FOLDER}/{title}_audio.mp3", format="mp3")
 print(f"Successfully downloaded audio of {title}")
 
 # Delete video
